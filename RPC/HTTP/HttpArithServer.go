1a332341a45406cd2c05d4947d491e406f5ef44a
GerogeZero <zero_geroge@outlook.com>
2015-08-14 22:11:18 +0800

    HTTP RPC Server
    Test HTTP RPC Server

---
+++ RPC/HTTP/HttpArithServer.go
@@ -0,0 +1,45 @@
+//Copyright ©Jan Newmarch, jan@newmarch.name. All rights reserved
+// HttpArithServer
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/rpc"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+
+func main() {
+	t := new(Arith)
+	rpc.Register(t)
+	rpc.HandleHTTP()
+
+	err := http.ListenAndServe(":1234", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
